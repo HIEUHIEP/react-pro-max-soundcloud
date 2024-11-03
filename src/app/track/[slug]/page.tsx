fe2ce0d46aed34e08afc10eb2cf4ebaddb35e513
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-03 19:51:51 +0700

    video 106 Search Param (useSearchParams)

--- src/app/track/[slug]/page.tsx
+++ src/app/track/[slug]/page.tsx
@@ -1,6 +1,11 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+
 
 const DetailTrackPage = (props: any) => {
     const { param } = props;
+    const searchParams = useSearchParams()
+    const search = searchParams.get('audio')
     return (
         <div>
             DetailTrackPage
