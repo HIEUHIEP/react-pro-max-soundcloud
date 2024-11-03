e06148188c6ab97c0e273c8f2ddbacd88fe0011e
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-03 21:50:37 +0700

    video 113 su dung useMemo hook

--- src/utils/customHook.ts
+++ src/utils/customHook.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 
 const useHasMounted = () => {
 
@@ -9,4 +10,29 @@ const useHasMounted = () => {
     return hasMounted;
 }
 
-export { useHasMounted }
+// WaveSurfer hook
+const useWavesurfer = (
+    containerRef: React.RefObject<HTMLDivElement>,
+    options: Omit<WaveSurferOptions, 'container'>) => {
+    const [wavesurfer, setWavesurfer] = useState<any>(null)
+
+    // Initialize wavesurfer when the container mounts
+    // or any of the props change
+    useEffect(() => {
+        if (!containerRef.current) return
+        const ws = WaveSurfer.create({
+            ...options,
+            container: containerRef.current,
+        })
+
+        setWavesurfer(ws)
+
+        return () => {
+            ws.destroy()
+        }
+    }, [options, containerRef])
+
+    return wavesurfer
+}
+
+export { useHasMounted, useWavesurfer }
