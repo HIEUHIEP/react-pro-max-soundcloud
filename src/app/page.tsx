7fe55709c262bfec3ec0d740c6549c60c5e7642b
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-03 18:52:11 +0700

    video 103.2 Fetch Wrapper Type script to call API SS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
-import { sendRequestJS } from "@/utils/old.api";
+import { sendRequest } from "@/utils/api";
 
 export default async function HomePage() {
 
@@ -16,15 +16,16 @@ export default async function HomePage() {
   //   }),
   // })
 
-  const res = await sendRequestJS({
+  const res = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
     body: {
       category: "CHILL",
       limit: 1
     },
+
   })
-  console.log("check >>", res)
+
   return (
     <Container>
       <MainSlider />
