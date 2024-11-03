62a6019af95b98df86f1602c5c5391598c6af9c8
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-03 19:47:16 +0700

    video 105 Dynamic Route

---
+++ src/app/track/[slug]/page.tsx
@@ -0,0 +1,10 @@
+
+const DetailTrackPage = (props: any) => {
+    const { param } = props;
+    return (
+        <div>
+            DetailTrackPage
+        </div>
+    )
+}
+export default DetailTrackPage;
