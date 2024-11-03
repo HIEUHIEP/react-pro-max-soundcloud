a7c6924780ca6dba79cc49475f6c81497bc056a8
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-03 20:47:06 +0700

    video 111.3 Fetch track from remote server (backend)

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,10 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest, response: NextResponse) {
+    const url = new URL(request.url);
+    const fileName = new URLSearchParams(url.search)
 
 
-
-    return NextResponse.json({ data: "" })
+    // return NextResponse.json({ data: "" })
+    return await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/${fileName.get("audio")}`)
 }
--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -2,16 +2,18 @@
 import { useEffect } from 'react'
 import WaveSurfer from 'wavesurfer.js'
 import { useRef } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 const WaveTrack = () => {
     const containerRef = useRef<HTMLDivElement>(null);
-
+    const searchParams = useSearchParams()
+    const fileName = searchParams.get('audio')
     useEffect(() => {
         WaveSurfer.create({
             container: containerRef.current!, // ! chắc chắn có tồn tại
             waveColor: 'rgb(200, 0, 200)',
             progressColor: 'rgb(100, 0, 100)',
-            url: '/audio/hoidanit.mp3',
+            url: `/api?audio=${fileName}`,
         })
     }, [])
     return (
