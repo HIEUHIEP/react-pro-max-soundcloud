27692934a6aa510efa5611a76f93f89e78e86a7c
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-05 21:54:22 +0700

    video 135

---
+++ src/app/(guest)/auth/signin/page.tsx
@@ -0,0 +1,12 @@
+import AuthSignIn from "@/components/auth/auth.signin"
+
+const SignInPage = () => {
+    return (
+        // <AuthSignIn />
+        <div>
+            sign in page
+        </div>
+    )
+}
+
+export default SignInPage;
