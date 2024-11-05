a6b8a522b213abb59eeebd460338c182bd356733
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-05 22:04:03 +0700

    video 139

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <ThemeRegistry>
           <NextAuthWrapper>
-            <AppHeader />
             {children}
-            <AppFooter />
           </NextAuthWrapper>
         </ThemeRegistry>
       </body>
