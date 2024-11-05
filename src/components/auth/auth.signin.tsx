0758009c2c3caa3c70d4a0f1e364fee1d0ac5c84
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-05 22:01:15 +0700

    video 138

--- src/components/auth/auth.signin.tsx
+++ src/components/auth/auth.signin.tsx
@@ -7,7 +7,7 @@ import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
-
+import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 const AuthSignIn = (props: any) => {
@@ -143,6 +143,9 @@ const AuthSignIn = (props: any) => {
                                 sx={{
                                     cursor: "pointer",
                                     bgcolor: "orange"
+                                }}
+                                onClick={() => {
+                                    signIn("github")
                                 }}
                             >
                                 <GitHubIcon titleAccess="Login with Github" />
