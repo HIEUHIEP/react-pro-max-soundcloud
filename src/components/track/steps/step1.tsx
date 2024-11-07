d65a26e7e483f3027c69466d377aefeb607fd236
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-07 19:55:47 +0700

    video 147.2 Drag/Drop

--- src/components/track/steps/step1.tsx
+++ src/components/track/steps/step1.tsx
@@ -1,9 +1,21 @@
 'use client'
 import { FileWithPath, useDropzone } from 'react-dropzone';
 import './theme.css'
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { useCallback } from 'react';
+
 
 const Step1 = () => {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+        // Do something with the files
+    }, [])
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({ onDrop });
+
+
+
 
     const files = acceptedFiles.map((file: FileWithPath) => (
         <li key={file.path}>
@@ -15,6 +27,7 @@ const Step1 = () => {
         <section className="container">
             <div {...getRootProps({ className: 'dropzone' })}>
                 <input {...getInputProps()} />
+                <InputFileUpload />
                 <p>Drag 'n' drop some files here, or click to select files</p>
             </div>
             <aside>
@@ -26,3 +39,35 @@ const Step1 = () => {
 }
 
 export default Step1;
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+export function InputFileUpload() {
+    return (
+        <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+            onClick={(event) => { event.preventDefault() }}
+        >
+            Upload files
+            <VisuallyHiddenInput
+                type="file"
+                onChange={(event) => console.log(event.target.files)}
+                multiple
+            />
+        </Button>
+    );
+}
