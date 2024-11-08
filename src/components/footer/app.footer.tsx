742fe2021b8333e705bd7195690567f0c5bdf287
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-08 21:08:49 +0700

    video 156 customize track footer

--- src/components/footer/app.footer.tsx
+++ src/components/footer/app.footer.tsx
@@ -8,25 +8,33 @@ import { useHasMounted } from "@/utils/customHook";
 const AppFooter = () => {
     const hasMounted = useHasMounted();
     if (!hasMounted) return (<></>);
-    console.log("check: ", process.env.NEXT_PUBLIC_BACKEND_URL)
+    // console.log("check: ", process.env.NEXT_PUBLIC_BACKEND_URL)
     return (
-        <AppBar position="fixed" sx={{ top: 'auto', bottom: 0, backgroundColor: "#f2f2f2" }}>
-            <Container sx={{ display: "flex", gap: 10 }}>
-                <AudioPlayer
-                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
-                    volume={0.5}
-                    style={{
-                        boxShadow: "unset",
-                        background: "#f2f2f2"
-                    }}
-                // Try other props!
-                />
-                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "start", justifyContent: "center", minWidth: "100px" }}>
-                    <Box sx={{ color: "#ccc" }}> Arthor </Box>
-                    <Box sx={{ color: "black" }}> Name song </Box>
-                </Box>
-            </Container >
-        </AppBar>
+        <div style={{ marginBottom: 100 }}>
+            <AppBar position="fixed" sx={{ top: 'auto', bottom: 0, backgroundColor: "#f2f2f2", }}>
+                <Container sx={{
+                    display: "flex", gap: 10, ".rhap_main": {
+                        gap: "30px"
+                    }
+                }}>
+                    <AudioPlayer
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
+                        volume={0.5}
+                        style={{
+                            boxShadow: "unset",
+                            background: "#f2f2f2"
+                        }}
+                        layout="horizontal-reverse"
+                    // Try other props!
+                    />
+                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "start", justifyContent: "center", minWidth: "100px" }}>
+                        <Box sx={{ color: "#ccc" }}> Arthor </Box>
+                        <Box sx={{ color: "black" }}> Name song </Box>
+                    </Box>
+                </Container >
+            </AppBar>
+        </div>
+
 
     );
 }
