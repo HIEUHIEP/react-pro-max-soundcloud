ade0c909ea3c389c34e8e06e8de7a6db551bb9a9
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-10 13:36:32 +0700

    video 171 Metadata title/description

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,6 +1,12 @@
 
 import AppFooter from '@/components/footer/app.footer';
 import AppHeader from '@/components/header/app.header';
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Title Layout',
+    description: 'description Layout',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
--- src/app/(user)/like/page.tsx
+++ src/app/(user)/like/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Title like page',
+    description: 'description like page',
+}
 
 const LikePage = () => {
     return (
